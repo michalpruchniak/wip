a18053912f9d26ec7f2a59aaed1531ae18be24c9
Michał Pruchniak <goldenofheart@gmail.com>
2023-08-01 18:51:29 +0200

    Adding login endpoint and saving jwt token in httpOnlyCookie

---
+++ backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function hello()
+    {
+        return 'Hello Worlddd';
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return \response([
+                'error' => 'Invalid Credentialis!'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60 * 48);
+
+        return \response([
+            'jwt' => $token
+        ])->withCookie($cookie);
+    }
+
+    public function userDetails(Request $request)
+    {
+        return $request->user();
+    }
+}
