31de26b87c0437e15421829f8d03749a8671c4ef
Michał Pruchniak <goldenofheart@gmail.com>
2023-08-02 12:51:09 +0200

    Create mock for testing login requests

--- frontend/src/Pages/__tests__/Login.test.js
+++ frontend/src/Pages/__tests__/Login.test.js
@@ -1,5 +1,12 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import Login from "../Login";
+import axios from "axios";
+import mockAxios from "axios-mock-adapter";
+
+import jwt from "../../../__mocks__/jwt.json";
+import invalidCredentials from "../../../__mocks__/invalidCredentials.json";
+
+const axiosMock = new mockAxios(axios);
 
 test("render form page", () => {
   render(<Login />);
@@ -41,4 +48,41 @@ test("check login validation", async () => {
 
   expect(errorAlertEmailPattern).toBeInTheDocument();
   expect(errorAlertPasswordPattern).toBeInTheDocument();
+});
+
+test("correclty logged-in", async () => {
+  axiosMock.onPost("/login").reply(200, { jwt: jwt.jwt });
+  render(<Login />);
+  const emailInput = screen.getByLabelText("Adres email");
+  const passwordInput = screen.getByLabelText("Hasło");
+  const submitButton = screen.getByRole("submit", { value: "Zaloguj" });
+
+  fireEvent.change(emailInput, { target: { value: "admin@admin.pl" } });
+  fireEvent.change(passwordInput, { target: { value: "password" } });
+  fireEvent.click(submitButton);
+
+  await waitFor(() => {
+    screen.getByText("Udało Ci się zalogować na konto.");
+  });
+});
+
+test("wrong creditionals", async () => {
+  axiosMock.onPost("/login").reply(400, { error: invalidCredentials.error });
+
+  render(<Login />);
+  const emailInput = screen.getByLabelText("Adres email");
+  const passwordInput = screen.getByLabelText("Hasło");
+  const submitButton = screen.getByRole("submit", { value: "Zaloguj" });
+
+  fireEvent.change(emailInput, { target: { value: "admin@admin.pl" } });
+  fireEvent.change(passwordInput, { target: { value: "hello world" } });
+  fireEvent.click(submitButton);
+
+  await waitFor(() => {
+    expect(
+      screen.getByText(
+        "Nie udało się zalogować. Prawdopodobnie podałeś zły adres email lub hasło."
+      )
+    ).toBeInTheDocument();
+  });
 });
