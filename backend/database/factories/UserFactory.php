25fb160583b2e7d77a06beeedc2e7e126c75a799
Michał Pruchniak <goldenofheart@gmail.com>
2023-08-04 11:21:33 +0200

    Create Profile model with User into factory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Profile;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -17,9 +18,19 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $email = fake()->unique()->safeEmail();
+        $profile = Profile::create([
+            'name' => fake()->name,
+            'lastname' => fake()->lastname,
+            'email' => $email,
+            'job' => 1,
+            'testing_systems' => 'Lorem ipsum',
+            'raporting_systems' => 'Lorem ipsum',
+        ]);
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'profile_id' => $profile->id,
+            'email' => $email,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
