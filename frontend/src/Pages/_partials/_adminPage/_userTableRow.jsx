6222e9139de70adb50088f080dd0f086bf190ab9
Michał Pruchniak <goldenofheart@gmail.com>
2023-08-05 17:58:40 +0200

    Add remove asking

--- frontend/src/Pages/_partials/_adminPage/_userTableRow.jsx
+++ frontend/src/Pages/_partials/_adminPage/_userTableRow.jsx
@@ -1,10 +1,14 @@
 import axios from "axios";
 import UsersStore from "../../../Config/UsersStore";
 import { observer } from "mobx-react-lite";
+import { useState } from "react";
 const UserTableRow = ({ user, key, deleted }) => {
+  const [ask, setAsk] = useState(false);
+
   const delUser = (id) => {
+    setAsk(false);
     axios
-      .delete("/delete/" + id)
+      .delete(`/delete/${id}`)
       .then(() => {
         UsersStore.delUser(id);
         deleted(true);
@@ -13,22 +17,47 @@ const UserTableRow = ({ user, key, deleted }) => {
         deleted(false);
       });
   };
-
+  const AskRemove = () => {
+    return (
+      <tr>
+        <td colSpan={6} className="pt-4 pb-3">
+          <h4 className="text-center">
+            Czy na pewno chcesz usunąć tego usera?
+          </h4>
+          <div
+            class="btn-group d-flex justify-content-center"
+            role="group"
+            aria-label="Basic example"
+          >
+            <a className="btn btn-danger" onClick={() => delUser(user.id)}>
+              Usuń
+            </a>
+            <a className="btn btn-primary" onClick={() => setAsk(false)}>
+              Anuluj
+            </a>
+          </div>
+        </td>
+      </tr>
+    );
+  };
   return (
-    <tr key={key}>
-      <td>{user.profile.name}</td>
-      <td>{user.profile.lastname}</td>
-      <td>{user.profile.email}</td>
-      <td>{user.profile.job}</td>
-      <td>{user.is_admin === 1 ? "Tak" : "Nie"}</td>
-      <td>
-        <i
-          className="fa-solid fa-pen-to-square"
-          style={{ marginRight: "10px" }}
-        ></i>
-        <i className="fa-solid fa-trash" onClick={() => delUser(user.id)}></i>
-      </td>
-    </tr>
+    <>
+      <tr key={key}>
+        <td>{user.profile.name}</td>
+        <td>{user.profile.lastname}</td>
+        <td>{user.profile.email}</td>
+        <td>{user.profile.job}</td>
+        <td>{user.is_admin === 1 ? "Tak" : "Nie"}</td>
+        <td>
+          <i
+            className="fa-solid fa-pen-to-square"
+            style={{ marginRight: "10px" }}
+          ></i>
+          <i className="fa-solid fa-trash" onClick={() => setAsk(!ask)}></i>
+        </td>
+      </tr>
+      {ask && <AskRemove />}
+    </>
   );
 };
 
